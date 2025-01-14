67fb86fb075d4c77cf55cf61dd665b01cf27b20e
kevinsilva <kevinsilva.j@gmail.com>
2025-01-14 10:17:25 +0000

    first commit

---
+++ src/main/java/pt/upskill/upfest/UpfestApplication.java
@@ -0,0 +1,13 @@
+package pt.upskill.upfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpfestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/upskill/upfest/UpfestApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.upskill.upfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
