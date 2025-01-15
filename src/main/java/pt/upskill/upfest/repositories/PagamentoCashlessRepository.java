b120a721dcdafbffff53a4b4ccc525601847cb9c
luciabarrela <lucia.sofiabb@gmail.com>
2025-01-15 15:43:39 +0000

    Apagar últimas alterações todas

--- src/main/java/pt/upskill/upfest/repositories/PagamentoCashlessRepository.java
+++
@@ -1,8 +0,0 @@
-package pt.upskill.upfest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pt.upskill.upfest.entities.PagamentoCashless;
-
-public interface PagamentoCashlessRepository extends JpaRepository<PagamentoCashless, Long> {
-}
-
--- src/main/java/pt/upskill/upfest/services/PagamentoCashlessService.java
+++
@@ -1,9 +0,0 @@
-package pt.upskill.upfest.services;
-
-import pt.upskill.upfest.entities.Pagamento;
-import pt.upskill.upfest.entities.PagamentoCashless;
-
-public interface PagamentoCashlessService {
-    PagamentoCashless registrarPagamento(Long contaId, Pagamento pagamento);
-}
-
